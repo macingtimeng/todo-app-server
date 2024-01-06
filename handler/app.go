6fa3c8b834d1a2f36758819ca72e4ba445cd364c
yusrililhm <yusrililham62@gmail.com>
2024-01-06 16:02:08 +0700

    [app] add: todo endpoints

--- handler/app.go
+++ handler/app.go
@@ -3,11 +3,14 @@ package handler
 import (
 	"fmt"
 
+	"todo-app/handler/todos_handler"
 	"todo-app/handler/users_handler"
 	"todo-app/infra/config"
 	"todo-app/infra/db"
+	"todo-app/repo/todos_repo/todos_pg"
 	"todo-app/repo/users_repo/users_pg"
 	"todo-app/service/auth_service"
+	"todo-app/service/todos_service"
 	"todo-app/service/users_service"
 
 	"github.com/gofiber/fiber/v2"
@@ -24,7 +27,11 @@ func StartApp() {
 	userService := users_service.NewUserService(userRepo)
 	userHandler := users_handler.NewUserHandler(userService)
 
-	authService := auth_service.NewAuthService(userRepo)
+	todoRepo := todos_pg.NewTodoRepo(db)
+	todoService := todos_service.NewTodoService(todoRepo)
+	todoHandler := todos_handler.NewTodoHandler(todoService)
+
+	authService := auth_service.NewAuthService(userRepo, todoRepo)
 
 	app := fiber.New()
 	app.Use(logger.New())
@@ -46,6 +53,12 @@ func StartApp() {
 	app.Post("/users/login", userHandler.Login)
 	app.Get("/users/profile", authService.Authentication(), userHandler.Profile)
 	app.Patch("/users/modify", authService.Authentication(), userHandler.Modify)
+
+	app.Post("/todos", authService.Authentication(), todoHandler.Add)
+	app.Get("/todos", authService.Authentication(), todoHandler.Fetch)
+	app.Delete("/todos/:todoId", authService.Authentication(), authService.Authorization(), todoHandler.Delete)
+	app.Get("/todos/:todoId", authService.Authentication(), authService.Authorization(), todoHandler.Detail)
+	app.Patch("/todos/:todoId", authService.Authentication(), authService.Authorization(), todoHandler.Modify)
 
 	app.Listen(":" + config.AppConfig().Port)
 }
