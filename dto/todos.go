7293fd1050e43295dbaabc723038ef7bfecd8345
yusrililhm <yusrililham62@gmail.com>
2024-01-06 16:03:17 +0700

    [dto] add: todo dto

--- dto/todos.go
+++ dto/todos.go
@@ -1,1 +1,36 @@
 package dto
+
+import (
+	"time"
+	"todo-app/entity"
+)
+
+type TodoResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Data    any    `json:"data"`
+}
+
+type AddTodo struct {
+	Todos string `json:"todos" valid:"required~ Todos can't be empty"`
+}
+
+type ModifyTodo struct {
+	Todos  string `json:"todos" valid:"required~ Todos can't be empty"`
+	Status bool   `json:"status"`
+}
+
+func (m *ModifyTodo) ModifyTodoToEntity() *entity.Todo {
+	return &entity.Todo{
+		Todos:  m.Todos,
+		Status: m.Status,
+	}
+}
+
+type Todo struct {
+	Id        uint      `json:"id"`
+	Todos     string    `json:"todos"`
+	Status    bool      `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
